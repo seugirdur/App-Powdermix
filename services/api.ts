043adc72baf303684eae909b5f859f5a36d9cc42
seugirdur <gabriel.lima137170@gmail.com>
2022-12-11 12:14:20 -0300

    arrumando caminho api

--- services/api.ts
+++ services/api.ts
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  //testando com api local
-  baseURL: 'http://192.168.1.105:8080',
+  // testando com api local
+  // baseURL: 'http://192.168.1.105:8080',
   // baseURL: 'http://192.168.175.42:8080',
 
   //teste com api da fly.io
   // baseURL: 'https://powdermixfly.fly.dev',
 
   //api da railway
-  // baseURL: 'https://steady-sidewalk-production.up.railway.app',
+  baseURL: 'https://steady-sidewalk-production.up.railway.app',
 
   //api da aws
   // baseURL: 'http://100.26.195.168:8080',
